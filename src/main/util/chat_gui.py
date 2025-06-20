85f6f23fe716d81708fe5e2e3c5a0a792ef9147b
hosmanadam <github@adamsinbox.com>
2025-06-20 05:03:50 +0200

    Handle graph response

--- src/main/util/chat_gui.py
+++ src/main/util/chat_gui.py
@@ -2,22 +2,32 @@ import logging
 
 import chainlit as cl
 from chainlit.input_widget import Select
+from langchain_core.messages import HumanMessage
 
-from src.main.rag import book_1
+from src.main.rag import general_1
 from src.main.util import rag_loader
 
 _logger = logging.getLogger(__name__)
 
 setting_rag_module_name = "rag_module_name"
-rag_module_name = book_1.__name__.split(".")[-1]
+rag_module_name = general_1.__name__.split(".")[-1]
 rag_chain = rag_loader.load_chain(rag_module_name)
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
     _logger.info(f"Message from user: {message.content}")
-    response = rag_chain.invoke(message.content)
-    await cl.Message(response["answer"]).send()
+    try:
+        # Assume simple chain
+        response = rag_chain.invoke(message.content)["answer"]
+    except ValueError:
+        # Turns out it's a graph, needs to be called differently # HACK
+        end_state = rag_chain.invoke(
+            input={"messages": [HumanMessage(message.content)]},
+            config={"configurable": {"thread_id": cl.context.session.id}}
+        )
+        response = end_state["messages"][-1].content
+    await cl.Message(response).send()
 
 
 @cl.on_chat_start
