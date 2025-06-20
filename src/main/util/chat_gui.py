af12a7624af1e06405e9c86aeca4dc40ecbc10c6
hosmanadam <github@adamsinbox.com>
2025-06-20 06:44:39 +0200

    Clean out TODO

--- src/main/util/chat_gui.py
+++ src/main/util/chat_gui.py
@@ -58,27 +58,3 @@ async def on_settings_update(settings: dict):
         global rag_chain
         rag_chain = rag_loader.load_chain(selected_rag_module_name)
         await cl.Message(f"Switched to RAG module `{rag_module_name}`").send()
-
-# TODO: Use chat profiles to switch RAG module (instead of chat settings)
-#  - Because it makes for a cleaner UI
-#  - Depends on chat history (auth & custom persistence layer)
-
-# @cl.on_message
-# async def on_message(message: cl.Message):
-#     chat_profile = cl.user_session.get("chat_profile")
-#     chain = rag_loader.get(chat_profile)
-#     response = chain.invoke(message.content)
-#     await cl.Message(response["answer"]).send()
-#
-#
-# @cl.set_chat_profiles
-# async def set_chat_profiles():
-#     names = rag_loader.get_names()
-#     return [cl.ChatProfile(name, markdown_description=f"Chat over '{name}' RAG module") for name in names]
-#
-# @cl.on_chat_start
-# async def on_chat_start():
-#     chat_profile = cl.user_session.get("chat_profile")
-#     await cl.Message(
-#         content=f"Starting chat over RAG module `{rag_module_name}`.\n"
-#     ).send()
